b46866e1c404dfaf717c08057869330a23b310c8
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-01 12:36:18 -0600

    moved functions to separate file

--- collect_blocks.py
+++ collect_blocks.py
@@ -1,110 +1,55 @@
-from urllib.request import urlopen
 from time import perf_counter, strftime
 from datetime import datetime
 from dateutil import tz
 from dateutil.relativedelta import *
-import logging
 import json
 import os
 
-def load_json(filepath):
-    with open(filepath, 'r') as fp:
-        data = json.load(fp)
-    return data
+from functions import get_days, get_block_summaries, get_block, save_json, load_json
 
-def save_json(filepath, data):
-    with open(filepath, 'w') as output_file:
-        json.dump(data, output_file)
+# set start and end days
+time_period_days = 1
+end_day = datetime(year=2021, month=6, day=28, tzinfo = tz.gettz('Etc/GMT'))
+start_day = end_day - relativedelta(days=time_period_days)
 
-def get_days(start_day,end_day):
-    """
-    inputs:     two datetime objects
+days = get_days(start_day, end_day)
 
-    returns:    datetime objects for every day from start_day up to, but not including end_day
-    """
-    days = []
+# create a directory to store the block json files
+if not os.path.exists('block_data'):
+    os.mkdir('block_data')
 
-    current_day = start_day
-    while current_day < end_day:
-        days.append(current_day)
-        current_day += relativedelta(days=+1)
+for day in days:
+    day_string = day.strftime("%Y-%m-%d")
 
-    return days
+    # create a sub-directory for each day to help keep things organized
+    if not os.path.exists(f'block_data/{day_string}'):
+        os.mkdir(f'block_data/{day_string}')
 
-def get_block_summaries(day):
-    """
-    inputs:     a datetime object
-
-    returns:    basic information about each block added to the blockchain on the given day
-                including the block hash and block height
-    """
-    timestamp = int(day.timestamp() * 1000)
-
-    url = f'https://blockchain.info/blocks/{str(timestamp)}?format=json'
-    response = urlopen(url)
-
-    block_summaries = json.load(response)
-    return block_summaries
-
-def get_block(block_hash):
-    """
-    inputs:     a block hash
-
-    returns:    a dictionary containg all information related to the specifed block
-                including header information and a list of transactions contained within the block
-    """
-
-    url = f'https://blockchain.info/rawblock/{block_hash}'
-    response = urlopen(url)
-
-    block_data = json.load(response)
-    return block_data
-
-if __name__ == '__main__':
-
-    # set start and end days
-    time_period_days = 1
-    end_day = datetime(year=2021, month=6, day=28, tzinfo = tz.gettz('Etc/GMT'))
-    start_day = end_day - relativedelta(days=time_period_days)
+    print(f'collecting blocks from {day_string}\n')
     
-    days = get_days(start_day, end_day)
+    block_summaries = get_block_summaries(day)
+    num_blocks = len(block_summaries)
 
-    # create a directory to store the block json files
-    if not os.path.exists('block_data'):
-        os.mkdir('block_data')
+    print(f'collected block summary file containing {num_blocks} blocks')
 
-    for day in days:
-        day_string = day.strftime("%Y-%m-%d")
+    file_name = f'block_data/{day_string}/blocks_{day_string}.json'
+    save_json(file_name,block_summaries)
 
-        # create a sub-directory for each day to help keep things organized
-        if not os.path.exists(f'block_data/{day_string}'):
-            os.mkdir(f'block_data/{day_string}')
+    n = 1
+    for block in block_summaries:
+        t1 = perf_counter()
+        block_hash = block.get('hash')
 
-        print(f'collecting blocks from {day_string}\n')
+        block_data = get_block(block_hash)
+
+        filename = f'block_data/{day_string}/{block_hash}.json'
+        save_json(filename, block_data)
+
+        t2 = perf_counter()
+        block_time = t2-t1 
+        print(f'collected block {block_hash} ({n}/{num_blocks}) in {block_time:.2f}s')
         
-        block_summaries = get_block_summaries(day)
-        num_blocks = len(block_summaries)
-
-        print(f'collected block summary file containing {num_blocks} blocks')
-
-        file_name = f'block_data/{day_string}/blocks_{day_string}.json'
-        save_json(file_name,block_summaries)
-
-        n = 1
-        for block in block_summaries:
-            t1 = perf_counter()
-            block_hash = block.get('hash')
-
-            block_data = get_block(block_hash)
-
-            filename = f'block_data/{day_string}/{block_hash}.json'
-            save_json(filename, block_data)
-
-            t2 = perf_counter()
-            block_time = t2-t1 
-            print(f'collected block {block_hash} ({n}/{num_blocks}) in {block_time:.2f}s')
-            
-            n += 1
+        n += 1
 
 
-        print()
+    print()
