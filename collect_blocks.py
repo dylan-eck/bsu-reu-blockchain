d22eae011b49d4be2e7b43cc72e3b25b05398ee8
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-01 16:11:57 -0600

    added error handling and replaced print statements with loggin

--- collect_blocks.py
+++ collect_blocks.py
@@ -1,55 +1,95 @@
-from time import perf_counter, strftime
+from time import perf_counter
 from datetime import datetime
 from dateutil import tz
 from dateutil.relativedelta import *
-import json
 import os
 
 from functions import get_days, get_block_summaries, get_block, save_json, load_json
 
+# configure logging
+import logging
+logging.basicConfig(filename='app.log',filemode='w',format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+
 # set start and end days
-time_period_days = 1
+time_period_days = 75
 end_day = datetime(year=2021, month=6, day=28, tzinfo = tz.gettz('Etc/GMT'))
 start_day = end_day - relativedelta(days=time_period_days)
 
 days = get_days(start_day, end_day)
 
 # create a directory to store the block json files
-if not os.path.exists('block_data'):
-    os.mkdir('block_data')
+try:
+    if not os.path.exists('block_data'):
+        os.mkdir('block_data')
+except:
+    message = 'could not create block_data directory'
+    logging.critical(message)
+    raise
+
+failed_days = set()
+failed_blocks = set()
 
 for day in days:
+    day_start = perf_counter()
+
     day_string = day.strftime("%Y-%m-%d")
 
-    # create a sub-directory for each day to help keep things organized
-    if not os.path.exists(f'block_data/{day_string}'):
-        os.mkdir(f'block_data/{day_string}')
+    try:
+        # create a sub-directory for each day to help keep things organized
+        if not os.path.exists(f'block_data/{day_string}'):
+            os.mkdir(f'block_data/{day_string}')
+    except:
+        failed_days.add(day_string)
 
-    print(f'collecting blocks from {day_string}\n')
+        message = f'could not create directory for day {day_string}'
+        logging.critical(message)
+        raise
+
+    logging.info(f'collecting blocks from {day_string}\n')
     
-    block_summaries = get_block_summaries(day)
-    num_blocks = len(block_summaries)
+    try:
+        block_summaries = get_block_summaries(day)
 
-    print(f'collected block summary file containing {num_blocks} blocks')
+    except:
+        failed_days.add(day_string)
 
-    file_name = f'block_data/{day_string}/blocks_{day_string}.json'
-    save_json(file_name,block_summaries)
+        logging.error(f'failed to load block summaries for {day_string}')
 
-    n = 1
-    for block in block_summaries:
-        t1 = perf_counter()
-        block_hash = block.get('hash')
+    else:
+        num_blocks = len(block_summaries)
 
-        block_data = get_block(block_hash)
+        logging.info(f'collected block summary file containing {num_blocks} blocks')
 
-        filename = f'block_data/{day_string}/{block_hash}.json'
-        save_json(filename, block_data)
+        file_name = f'block_data/{day_string}/blocks_{day_string}.json'
+        save_json(file_name,block_summaries)
 
-        t2 = perf_counter()
-        block_time = t2-t1 
-        print(f'collected block {block_hash} ({n}/{num_blocks}) in {block_time:.2f}s')
-        
-        n += 1
+        n = 1
+        for block in block_summaries:
+            block_start = perf_counter()
+            
+            block_hash = block.get('hash')
+
+            try:
+                block_data = get_block(block_hash)
+
+            except:
+                block_string = f'{block_hash}_{day_string}'
+
+                logging.error(f'failed to load block {block_hash}')
+
+            else:
+                filename = f'block_data/{day_string}/{block_hash}.json'
+                save_json(filename, block_data)
+
+                block_end = perf_counter()
+                block_time = block_end-block_start
+                logging.info(f'collected block {block_hash} ({n}/{num_blocks}) - processing time: {block_time:.2f}s')
+                
+                n += 1
+
+        day_end = perf_counter()
+        day_tiem = (day_end-day_start)/60
+        logging.info(f'\n collected {num_blocks} blocks from {day_string} - processing time: {block_time:.2f}\n')
 
 
-    print()
+
