a77dfcad7338c24f15c5aef6129e08915cf4ad53
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-07 12:26:38 -0400

    added partial implementation of transaction simplification

--- classify.py
+++ classify.py
@@ -16,13 +16,22 @@ def connectable(input_vals, output_vals, fee):
 def read_transaction(line):
     line = line.split(',')
     transaction_hash = line[0]
-    input_addrs = line[1].split(':')
-    input_vals = [int(x) for x in line[2].split(':')]
-    output_addrs = line[3].split(':')
-    output_vals = [int(x) for x in line[4].split(':')]
+    input_addresses = line[1].split(':')
+    input_values = [int(x) for x in line[2].split(':')]
+    output_addresses = line[3].split(':')
+    output_values = [int(x) for x in line[4].split(':')]
     transaction_fee = int(line[5])
 
-    return (transaction_hash, input_addrs, input_vals, output_addrs, output_vals, transaction_fee)
+    inputs = list(zip(input_addresses, input_values))
+    outputs = list(zip(output_addresses, output_values))
+
+    return [transaction_hash, inputs, outputs, transaction_fee]
+
+def remove_small_inputs(inputs, transaction_fee):
+    for (address, value) in inputs:
+        if value < transaction_fee:
+            inputs.remove((address, value))
+    return inputs
 
 t1 = perf_counter()
 
@@ -37,9 +46,7 @@ for (root, dirs, files) in os.walk('csv_files'):
 csv_file_paths = csv_file_paths[1:2]
 
 print('loading transactions')
-in_out_upper_limit = 11
-mtm_transactions = []
-total_transactions = 0
+transactions = []
 num_files = len(csv_file_paths)
 current_file = 1
 for file_path in csv_file_paths:
@@ -47,99 +54,119 @@ for file_path in csv_file_paths:
         input_file.readline()
         for line in input_file:
             transaction = read_transaction(line)
-            transaction_hash = transaction[0]
-            num_inputs = len(transaction[1])
-            num_outputs = len(transaction[3])
-
-            if num_inputs > 1 and num_inputs < in_out_upper_limit and num_outputs > 1 and num_outputs < in_out_upper_limit:
-                mtm_transactions.append(transaction)
+            transactions.append(transaction)
             
-            total_transactions += 1
         print(f'proccessed file {file_path} ({current_file}/{num_files})')
         current_file += 1
 
-print('\nclassifying transactions')
+transactions = transactions[:10000]
 
-total_mtm= len(mtm_transactions)
-num_simple = total_transactions - total_mtm
-num_separable = 0
-num_ambiguous = 0
-current_transaction = 1
-for transaction in mtm_transactions:
-    t_start = perf_counter()
+print('removing small inputs')
+num_affected_txs = 0
+total_inputs_removed = 0
+for transaction in transactions:
+    old_num_inputs = len(transaction[1])
+    old_num_outputs = len(transaction[2])
 
-    transaction_hash = transaction[0]
-    input_addrs = transaction[1]
-    input_vals = transaction[2]
-    output_addrs = transaction[3]
-    output_vals = transaction[4]
-    fee = transaction[5]
+    transaction[1] = remove_small_inputs(transaction[1],transaction[3])
 
-    input_sets = []
-    for i in range(1,len(input_vals)):
-        input_sets += list(combinations(input_vals,i))
+    new_num_inputs = len(transaction[1])
+    new_num_outputs = len(transaction[2])
+    
+    change = old_num_inputs - new_num_inputs
+    if change > 0:
+        num_affected_txs += 1
+        total_inputs_removed += change
 
-    output_sets = []
-    for i in range(1,len(output_vals)):
-        output_sets += list(combinations(output_vals,i))
+        old_cardinality = f'{old_num_inputs:>3}:{old_num_outputs:<3}'
+        new_cardinality = f'{new_num_inputs:>3}:{new_num_outputs:<3}'
+        print(f'removed {change:^3} small inputs from transaction {transaction[0]} old cardinality: {old_cardinality} new cardinality: {new_cardinality}')
+print(f'removed {total_inputs_removed:,} total inputs form {num_affected_txs:,} total transactions')
 
-    num_connectable_sets = 0
 
-    in_adj_list = dict()
-    out_adj_list = dict()
-    for output_set in output_sets:
-        for input_set in input_sets:
-            if connectable(input_set,output_set,fee):
-                num_connectable_sets += 1
-                if output_set in out_adj_list:
-                    out_adj_list[output_set].append(input_set)
-                else:
-                    out_adj_list[output_set] = [input_set]
 
-                if input_set in in_adj_list:
-                    in_adj_list[input_set].append(output_set)
-                else:
-                    in_adj_list[input_set] = [output_set]
+# print('\nclassifying transactions')
 
-    if num_connectable_sets > 2:
-        ambiguous = False
-        for input_set in in_adj_list:
-            output_sets = in_adj_list[input_set]
-            if len(input_sets) > 1:
-                ambiguous = True
+# total_mtm= len(mtm_transactions)
+# num_simple = total_transactions - total_mtm
+# num_separable = 0
+# num_ambiguous = 0
+# current_transaction = 1
+# for transaction in mtm_transactions:
+#     t_start = perf_counter()
 
-        for output_set in out_adj_list:
-            input_sets = out_adj_list[output_set]
-            if len(input_sets) > 1:
-                ambiguous = True
+#     transaction_hash = transaction[0]
+#     input_addrs = transaction[1]
+#     input_vals = transaction[2]
+#     output_addrs = transaction[3]
+#     output_vals = transaction[4]
+#     fee = transaction[5]
 
-        if ambiguous:
-            type = 'ambiguous'
-            num_ambiguous += 1
-        else:
-            type = 'separable'
-            num_separable += 1
+#     input_sets = []
+#     for i in range(1,len(input_vals)):
+#         input_sets += list(combinations(input_vals,i))
 
-    else:
-        type = 'simple'
-        num_simple += 1
+#     output_sets = []
+#     for i in range(1,len(output_vals)):
+#         output_sets += list(combinations(output_vals,i))
 
-    num_inputs = len(input_addrs)
-    num_outputs = len(output_addrs)
-    t_finish = perf_counter()
-    print(f'classified transaction {transaction_hash} cardinality: {num_inputs:3}:{num_outputs:<3} type: {type:10} #: {current_transaction:8,}/{total_mtm:<8,} time: {t_finish-t_start:4.2e}s')
-    current_transaction += 1
+#     num_connectable_sets = 0
 
-print()
+#     in_adj_list = dict()
+#     out_adj_list = dict()
+#     for output_set in output_sets:
+#         for input_set in input_sets:
+#             if connectable(input_set,output_set,fee):
+#                 num_connectable_sets += 1
+#                 if output_set in out_adj_list:
+#                     out_adj_list[output_set].append(input_set)
+#                 else:
+#                     out_adj_list[output_set] = [input_set]
 
-percent_simple = (num_simple / total_transactions) * 100
-percent_separable = (num_separable / total_transactions) * 100
-percent_ambiguous = (num_ambiguous / total_transactions) * 100
+#                 if input_set in in_adj_list:
+#                     in_adj_list[input_set].append(output_set)
+#                 else:
+#                     in_adj_list[input_set] = [output_set]
 
-t2 = perf_counter()
+#     if num_connectable_sets > 2:
+#         ambiguous = False
+#         for input_set in in_adj_list:
+#             output_sets = in_adj_list[input_set]
+#             if len(input_sets) > 1:
+#                 ambiguous = True
 
-print(f'total transactions: {total_transactions:,}')
-print(f'            simple: {num_simple:,} ({percent_simple:.2f}%)')
-print(f'         separable: {num_separable:,} ({percent_separable:.2f}%)')
-print(f'         ambiguous: {num_ambiguous:,} ({percent_ambiguous:.2f}%)')
-print(f'\n    execution time: {(t2-t1)/60:.2f} minutes')
+#         for output_set in out_adj_list:
+#             input_sets = out_adj_list[output_set]
+#             if len(input_sets) > 1:
+#                 ambiguous = True
+
+#         if ambiguous:
+#             type = 'ambiguous'
+#             num_ambiguous += 1
+#         else:
+#             type = 'separable'
+#             num_separable += 1
+
+#     else:
+#         type = 'simple'
+#         num_simple += 1
+
+#     num_inputs = len(input_addrs)
+#     num_outputs = len(output_addrs)
+#     t_finish = perf_counter()
+#     print(f'classified transaction {transaction_hash} cardinality: {num_inputs:3}:{num_outputs:<3} type: {type:10} #: {current_transaction:8,}/{total_mtm:<8,} time: {t_finish-t_start:4.2e}s')
+#     current_transaction += 1
+
+# print()
+
+# percent_simple = (num_simple / total_transactions) * 100
+# percent_separable = (num_separable / total_transactions) * 100
+# percent_ambiguous = (num_ambiguous / total_transactions) * 100
+
+# t2 = perf_counter()
+
+# print(f'total transactions: {total_transactions:,}')
+# print(f'            simple: {num_simple:,} ({percent_simple:.2f}%)')
+# print(f'         separable: {num_separable:,} ({percent_separable:.2f}%)')
+# print(f'         ambiguous: {num_ambiguous:,} ({percent_ambiguous:.2f}%)')
+# print(f'\n    execution time: {(t2-t1)/60:.2f} minutes')
