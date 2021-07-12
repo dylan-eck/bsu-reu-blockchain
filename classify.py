f9718b26be2da7b95c83d984abd5ce8e023ba96e
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-12 12:53:50 -0500

    removed info dump from end of classify.py

--- classify.py
+++ classify.py
@@ -3,54 +3,53 @@ from time import perf_counter
 import os
 import re
 
-def read_transaction(line):
-    line = line.split(',')
-    hash = line[0]
+def transactions_from_partitions(transaction, partitions):
+    transactions = []
 
-    input_addresses = line[1].split(':')
-    if input_addresses == ['coinbase']:
-        return None
-    input_values = [int(x) for x in line[2].split(':')]
-    inputs = list(zip(input_addresses, input_values))
+    hash = transaction.hash
+    fee = transaction.fee
 
-    output_addresses = line[3].split(':')
-    output_values = [int(x) for x in line[4].split(':')]
-    outputs = list(zip(output_addresses, output_values))
+    for partition in partitions:
+        inputs = partition[0]
+        outputs = partition[1]
+        partition_size = len(inputs)
 
-    fee = int(line[5])
+        for i in range(partition_size):
+            transaction = Transaction(hash,inputs[i],outputs[i],fee)
 
-    transaction = Transaction(hash, inputs, outputs, fee)
-    return transaction
+    return transactions
 
-# def write_transaction(file, transaction):
-#     hash = transaction[0]
-#     input_addresses = [input[0] for input in transaction[1]]
-#     input_values = [input[1] for input in transaction[1]]
-#     output_addresses = [output[0] for output in transaction[2]]
-#     output_values = [output[1] for output in transaction[2]]
-#     fee = transaction[3]
+def locate_files(direcotry, pattern):
+    file_paths = []
+    for (root, dirs, files) in os.walk(direcotry):
+        for file in files:
+            pattern = re.compile(pattern)
+            if pattern.match(file):
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+    return file_paths
 
-#     file.write(f'{hash},{input_addresses},{input_values},{output_addresses},{output_values},{fee}\n')
+def load_transactions_from_csv(csv_file_path):
+    transactions = []
 
-def is_large(transaction):
-    size_limit = 8
-    if len(transaction.inputs) >= size_limit or len(transaction.outputs) >= size_limit:
-        return True
-    else:
-        return False
+    with open(csv_file_path, 'r') as input_file:
+        input_file.readline()
+        for line in input_file:
+            transaction = Transaction()
+            transaction.from_csv_string(line)
+
+            if not transaction.is_coinbase():
+                transactions.append(transaction)
+
+    return transactions
+    
 
 program_start = perf_counter()
 
 # --- locate csv files ---
 print('locating csv files')
-csv_file_paths = []
-for (root, dirs, files) in os.walk('csv_files'):
-        for file in files:
-            pattern = re.compile("transactions_[0-9]*.csv$")
-            if pattern.match(file):
-                file_path = os.path.join(root, file)
-                csv_file_paths.append(file_path)
-csv_file_paths = csv_file_paths[1:2]
+csv_file_paths = locate_files('csv_files', "transactions_[0-9]*.csv$")
+csv_file_paths = csv_file_paths[0:1]
 
 # --- load transactions ---
 print('\nloading transactions')
@@ -58,16 +57,9 @@ transactions = []
 num_files = len(csv_file_paths)
 current_file = 1
 for file_path in csv_file_paths:
-    with open(file_path, 'r') as input_file:
-        input_file.readline()
-        for line in input_file:
-            transaction = read_transaction(line)
-
-            if transaction:
-                transactions.append(transaction)
-            
-        print(f'proccessed file {file_path} ({current_file}/{num_files})')
-        current_file += 1
+    transactions += load_transactions_from_csv(file_path) 
+    print(f'proccessed file {file_path} ({current_file}/{num_files})')
+    current_file += 1
 
 transactions = transactions[:10000]
 
@@ -87,13 +79,15 @@ timing_dict = {
 
 for transaction in transactions:
     classif_start = perf_counter()
+
+    tx_size_limit = 8
     
     if len(transaction.inputs) == 1 or len(transaction.outputs) == 1:
         num_partitions = 0
         transaction.type = 'simple'
         num_simple += 1
 
-    elif is_large(transaction):
+    elif len(transaction.inputs) >= tx_size_limit or len(transaction.outputs) >= tx_size_limit:
         transaction.type = 'intractable'
         num_intract += 1
 
@@ -113,50 +107,3 @@ for transaction in transactions:
         else:
             transaction.type = 'simple'
             num_simple += 1
-
-    # print information about the most recent transaction that was proccessed
-    classif_end = perf_counter()
-    timing_dict[transaction.type].append(classif_end - classif_start)
-    num_word = 'partition' if num_partitions == 1 else 'partitions'
-    cardinality = f'{len(transaction.inputs):>3}:{len(transaction.outputs):<3}'
-    print(f'found {num_partitions:4} {num_word:10} for transaction {transaction.hash} - cardinality: {cardinality} classification: {transaction.type:11} time: {classif_end - classif_start:.2e}s')
-
-# --- print information summarizing all of the transactions that were processed ---
-total_transactions = len(transactions)
-percent_simple = (num_simple / total_transactions) * 100
-percent_separable = (num_separable / total_transactions) * 100
-percent_ambiguous = (num_ambiguous / total_transactions) * 100
-percent_intract = (num_intract / total_transactions) * 100
-
-simple_time = sum(timing_dict['simple'])
-simple_avg = simple_time / num_simple
-simple_worst = max(timing_dict['simple'])
-
-separable_time = sum(timing_dict['separable'])
-separable_avg = separable_time / num_separable
-separable_worst = max(timing_dict['separable'])
-
-ambiguous_time = sum(timing_dict['ambiguous'])
-ambiguous_avg = ambiguous_time / num_ambiguous
-ambiguous_worst = max(timing_dict['ambiguous'])
-
-intract_time = sum(timing_dict['intractable'])
-intract_avg = intract_time / num_intract
-intract_worst = max(timing_dict['intractable'])
-
-program_end = perf_counter()
-execution_time = (program_end - program_start)
-
-print()
-print(f'  total transactions: {total_transactions:^7,}')
-print(f'              simple: {num_simple:^7,} ({percent_simple:5.2f}%)')
-print(f'           separable: {num_separable:^7,} ({percent_separable:5.2f}%)')
-print(f'           ambiguous: {num_ambiguous:^7,} ({percent_ambiguous:5.2f}%)')
-print(f'         intractable: {num_intract:^7,} ({percent_intract:5.2f}%)')
-
-print()
-print(f'total execution time: {execution_time/60:07.2f} minutes')
-print(f'              simple: {simple_time:7.2f}s ({simple_avg:5.2f}s average) ({simple_worst:5.2f}s worst)')
-print(f'           separable: {separable_time:7.2f}s ({separable_avg:5.2f}s average) ({separable_worst:5.2f}s worst)')
-print(f'           ambiguous: {ambiguous_time:7.2f}s ({ambiguous_avg:5.2f}s average) ({ambiguous_worst:5.2f}s worst)')
-print(f'         intractable: {intract_time:7.2f}s ({intract_avg:5.2f}s average) ({intract_worst:5.2f}s worst)')
--- untangle.py
+++ untangle.py
@@ -1,12 +1,15 @@
 from itertools import permutations
 
 class Transaction:
-	def __init__(self, hash, inputs, outputs, fee, type='unclassified'):
+	def __init__(self, hash='', inputs=[], outputs=[], fee=0, type='unclassified'):
 		self.hash = hash
 		self.inputs = inputs
 		self.outputs = outputs
 		self.fee = fee
 		self.type = type
+		
+	def is_coinbase(self):
+		return self.inputs == [('coinbase',)]
 
 	def from_csv_string(self, csv_string):
 		fields = csv_string.split(',')
@@ -14,14 +17,19 @@ class Transaction:
 		self.hash = fields[0]
 
 		input_addresses = fields[1].split(':')
-		input_values = fields[2].split(':')
+		
+		if fields[2]:
+			input_values = [int(x) for x in fields[2].split(':')]
+		else:
+			input_values = []
+
 		self.inputs = list(zip(input_addresses, input_values))
 
 		output_addresses = fields[3].split(':')
-		output_values = fields[4].split(':')
+		output_values = [int(x) for x in fields[4].split(':')]
 		self.outputs = list(zip(output_addresses, output_values))
 
-		self.fee = fields[5]
+		self.fee = int(fields[5])
 
 	def to_csv_string(self):
 		input_addresses = [input[0] for input in self.inputs]
