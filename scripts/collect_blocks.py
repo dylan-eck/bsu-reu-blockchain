db98a439be498031c2edd72e3c295beabc9c5963
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-14 10:26:52 -0500

    moved functions used only by collect_blocks.py back into collect_blocks.py

--- scripts/collect_blocks.py
+++ scripts/collect_blocks.py
@@ -5,16 +5,68 @@ inputs:     a date range (specified using a start date and end date) to collect 
 
 outputs:    json files for all blocks added from the start date up to and including the end date
 '''
-
 from time import perf_counter
 from datetime import datetime
 from dateutil import tz
 from dateutil.relativedelta import *
+from urllib.request import urlopen
+import json
 import logging
 import os
 import re
 
-from functions import get_days, get_block_summaries, get_block, save_json, load_json
+def get_days(start_day,end_day):
+    """
+    inputs:     two datetime objects
+
+    returns:    datetime objects for every day from start_day up to, but not including end_day
+    """
+    days = []
+
+    current_day = start_day
+    while current_day <= end_day:
+        days.append(current_day)
+        current_day += relativedelta(days=+1)
+
+    return days
+
+def get_block_summaries(day):
+    """
+    inputs:     a datetime object
+
+    returns:    basic information about each block added to the blockchain on the given day
+                including the block hash and block height
+    """
+    timestamp = int(day.timestamp() * 1000)
+
+    url = f'https://blockchain.info/blocks/{str(timestamp)}?format=json'
+    response = urlopen(url)
+
+    block_summaries = json.load(response)
+    return block_summaries
+
+def get_block(block_hash):
+    """
+    inputs:     a block hash
+
+    returns:    a dictionary containg all information related to the specifed block
+                including header information and a list of transactions contained within the block
+    """
+
+    url = f'https://blockchain.info/rawblock/{block_hash}'
+    response = urlopen(url)
+
+    block_data = json.load(response)
+    return block_data
+
+def load_json(filepath):
+    with open(filepath, 'r') as fp:
+        data = json.load(fp)
+    return data
+
+def save_json(filepath, data):
+    with open(filepath, 'w') as output_file:
+        json.dump(data, output_file)
 
 # configure logging
 if not os.path.exists('logs'):
