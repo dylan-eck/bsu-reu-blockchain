03f6afb2d694923fb2ffb8b22b648a7219d162a8
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-21 16:23:34 -0600

    selected addresses are now written to a file

--- scripts/select_addresses.py
+++ scripts/select_addresses.py
@@ -1,26 +1,32 @@
 import networkx as nx
-from networkx.classes.function import all_neighbors
+import random
 
-# load graph
-address_graph = nx.read_gpickle('../data_out/address_graph.pickle')
+def select_addresses(data_io_directory, graph_path):
+    # load graph
+    address_graph = nx.read_gpickle(graph_path)
 
-mtm_addrs = []
-non_mtm_addrs = []
-selected_addresses = []
+    mtm_addrs = []
+    selected_addresses = []
 
-for node in address_graph.nodes:
-    if address_graph.nodes[node]['in_mtm']:
-        mtm_addrs.append(node)
-    
-    else:
-        non_mtm_addrs.append(node)
+    for node in address_graph.nodes:
+        if address_graph.nodes[node]['in_mtm']:
+            mtm_addrs.append(node)
 
-for address in mtm_addrs:
-    if len(address_graph.adj[address]) > 0:
-        for neighbor in address_graph.adj[address]: 
-            if not address_graph.nodes[neighbor]['in_mtm']:
-                selected_addresses.append(neighbor)
+    for address in mtm_addrs:
+        if len(address_graph.adj[address]) > 0:
+            for neighbor in address_graph.adj[address]: 
+                if not address_graph.nodes[neighbor]['in_mtm']:
+                    selected_addresses.append(neighbor)
 
-print(f'      mtm addreses: {len(mtm_addrs):,}')
-print(f' non mtm addresses: {len(non_mtm_addrs):,}')
-print(f'selected addresses: {len(selected_addresses):,}')
+    n = 2000
+    selected_addresses = random.sample(selected_addresses, n)
+
+    output_directory = data_io_directory
+    with open(f'{output_directory}/selected_address', 'w') as output_file:
+        output_file.write('address')
+        for address in selected_addresses:
+            output_file.write(f'\n{address}')
+
+if __name__ == '__main__':
+    d = '../data_out'
+    select_addresses(d,f'{d}/address_graph.pickle')
