dcd3d0dffe9bbc87b0d861c6fd34d719b028ecea
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-21 09:15:34 -0600

    input and output directories are selected based on command line arguments

--- scripts/main.py
+++ scripts/main.py
@@ -14,4 +14,20 @@ parser.add_argument('-o', '-output',
                     )
 
 args = parser.parse_args()
-print(args.input_directory)
+
+DEFAULT_INPUT_DIRECTORY = '../block_data'
+DEFUALT_OUTPUT_DIRECTORY = '../csv_files'
+
+if args.input_directory:
+    input_directory = args.input_directory
+    print(f'using user-specified input directory {input_directory}')
+else:
+    input_directory = DEFAULT_INPUT_DIRECTORY
+    print(f'using default input directory {input_directory}')
+
+if args.output_directory:
+    output_directory = args.output_directory
+    print(f'using user-specified output directory {output_directory}')
+else:
+    output_directory = DEFUALT_OUTPUT_DIRECTORY
+    print(f'using default output directory {output_directory}')
