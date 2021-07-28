fb5fd0c6b3d7b918c94930e9efd9bfacdfb12b5a
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-28 00:00:33 -0600

    code cleanup

--- scripts/classify.py
+++ scripts/classify.py
@@ -9,15 +9,14 @@ from time import perf_counter
 import multiprocessing as mp
 import os
 
-from functions import get_file_names, load_transactions_from_csv, classify
+from functions import get_file_names_regex, load_transactions_from_csv, classify_transaction
 
-def classify_transactions(data_io_directory, file_pattern):
+def classify_transactions(data_io_directory, file_regex_pattern):
+    global indent
+    
     program_start = perf_counter()
 
-    indent = ''
-    if __name__ != '__main__':
-        indent = '    '
-
+    # utilize multiprocessing to speed up execution
     num_processes = mp.cpu_count()
     pool = mp.Pool(processes=num_processes)
     print(f'{indent}found {num_processes} available threads\n')
@@ -28,27 +27,28 @@ def classify_transactions(data_io_directory, file_pattern):
     if not os.path.exists(output_directory):
         os.mkdir(output_directory)
 
-    # --- locate csv files ---
+    # locate inputcsv files
     print(f'{indent}locating csv files... ', end='', flush=True)
-    csv_file_names = get_file_names(input_directory, file_pattern)
+    csv_file_names = get_file_names_regex(input_directory, file_regex_pattern)
     print('done\n')
 
-    # --- load transactions ---
     for file_name in csv_file_names:
         file_start = perf_counter()
 
         print(f'{indent}processing file raw_transactions_unclassified/{file_name}:\n')
+
         print(f'{indent}    loading transactions... ', end='', flush=True)
         transactions = load_transactions_from_csv(f'{input_directory}/{file_name}')    
         print('done')
 
         print(f'{indent}    classifying transactions... ', end='', flush=True)
-        classified_transactions = pool.map(classify, transactions)
+        classified_transactions = pool.map(classify_transaction, transactions)
         print('done')
 
         print(f'{indent}    writing new csv file... ', end='', flush=True)
         with open(f'{output_directory}/{file_name}', 'w') as output_file:
             output_file.write('transaction_hash,num_inputs,input_addresses,input_values,num_outputs,output_addresses,output_values,transaction_fee,transaction_class\n')
+            
             for transaction in classified_transactions:
                 output_file.write(transaction.to_csv_string())
         print('done')
@@ -57,8 +57,12 @@ def classify_transactions(data_io_directory, file_pattern):
         print(f'{indent}    finished in {file_end - file_start:.2f}s\n')
 
     program_end = perf_counter()
-    execution_time = (program_end-program_start)/60/60
-    print(f'{indent}execution time: {execution_time:.2f} hours')
+    execution_time_s = program_end-program_start
+    print(f'{indent}execution time: {execution_time_s:.2f}s')
+
+indent = ''
+if __name__ != '__main__':
+    indent = '    '
 
 if __name__ == '__main__':
     classify_transactions('../data_out')
