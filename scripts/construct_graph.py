4ffec38ab5d61cea9b7298f758a66115eaa570fb
Dylan Eck <dylaneck@mymail.mines.edu>
2021-07-28 00:54:20 -0600

    code cleanup

--- scripts/construct_graph.py
+++ scripts/construct_graph.py
@@ -4,7 +4,7 @@ from itertools import combinations
 import multiprocessing as mp
 import networkx as nx
 
-from functions import get_file_names, load_transactions_from_csv, profile
+from functions import get_file_names_regex, load_transactions_from_csv, profile_transactions
 
 def load_clusters(cluster_file_path):
     cluster_dict = {}
@@ -16,7 +16,11 @@ def load_clusters(cluster_file_path):
             address = fields[0]
             cluster = fields[1].strip()
 
-            cluster_dict[address] = cluster
+            if address in cluster_dict:
+                print('addresses may not be members of more than one cluster')
+
+            else:
+                cluster_dict[address] = cluster
         
     return cluster_dict
 
@@ -31,11 +35,12 @@ def link_cluster_addresses_isolated(cluster_dict, transaction):
     for address in (input_addresses + output_addresses):
 
         try:
+            # check to see if the address is a member of a cluster
             cluster = cluster_dict[address]
 
         except:
-            # print(f'{address} not a key in cluster_dict')
-            return edges
+            print(f'{address} not a key in cluster_dict')
+            continue
 
         if cluster in tx_cluster_dict:
             tx_cluster_dict[cluster].append(address)
@@ -43,7 +48,8 @@ def link_cluster_addresses_isolated(cluster_dict, transaction):
         else:
             tx_cluster_dict[cluster] = [address]
 
-    # check to see if any of the addresses in the graph belong to a common cluster
+    # check to see if any of the addresses in the transaction belong to a common cluster
+    # if they do, add edges between them
     for key in tx_cluster_dict:
         addresses = tx_cluster_dict[key]
         if len(addresses) > 1:
@@ -62,6 +68,7 @@ def get_nodes_and_edges(transaction, with_clusters=False, cluster_dict=None, lin
     num_inputs = len(input_addresses)
     num_outputs = len(output_addresses)
 
+    # add nodes to graph, keeping track of whether or not they are involved in any many-to-many transactions
     if num_inputs == 1 or num_outputs == 1:
         for i in input_addresses:
             for j in output_addresses:
@@ -102,32 +109,29 @@ def get_nodes_and_edges(transaction, with_clusters=False, cluster_dict=None, lin
             for edge in cluster_edges:
                 edges.append(edge)
 
-
-
-
     return (nodes, edges)
 
 def construct_graph(input_directory, output_directory, file_pattern, graph_name, with_clusters=False, cluster_file_path='', linked=False):
+    global indent # used for output formatting
+    
     program_start = perf_counter()
 
-    indent = ''
-    if __name__ != '__main__':
-        indent = '    '
-
+    # multiprocessing is used to speed up execution time
     threads = mp.cpu_count()
     pool = mp.Pool(processes=4)
     print(f'{indent}found {threads} available threads')
 
     print(f'{indent}locating input files... ', end='',flush=True)
-    csv_file_names = get_file_names(input_directory, file_pattern)
+    csv_file_names = get_file_names_regex(input_directory, file_pattern)
+    print('done\n')
 
     global cluster_dict
     if with_clusters:
+        print(f'{indent}loading cluster file... ', end='', flush=True)
         cluster_dict = load_clusters(cluster_file_path)
+        print('done')
     else:
         cluster_dict = None
-
-    print('done\n')
 
     graph = nx.Graph()
     
@@ -140,22 +144,21 @@ def construct_graph(input_directory, output_directory, file_pattern, graph_name,
         transactions = load_transactions_from_csv(f'{input_directory}/{file}')
         print('done')
 
-        temp = profile(transactions)
-        for (key, val) in temp.items():
+        tx_summary = profile_transactions(transactions)
+        for (key, val) in tx_summary.items():
             print(f'        {key:>14}: {val:,}')
         print()
 
         print(f'{indent}    collecting graph edges... ', end='', flush=True)
-        func = partial(get_nodes_and_edges, with_clusters=with_clusters, cluster_dict=cluster_dict)
-
-        res = pool.map(func, transactions)
+        func = partial(get_nodes_and_edges, with_clusters=with_clusters, cluster_dict=cluster_dict, linked=linked)
+        results = pool.map(func, transactions)
         print('done')
 
-        graph_nodes = [sl[0] for sl in res]
-        graph_edges = [sl[1] for sl in res]
+        # separate out nodes and eges
+        graph_nodes = [sublist[0] for sublist in results]
+        graph_edges = [sublist[1] for sublist in results]
 
         print(f'{indent}    adding edges to graph... ', end='', flush=True)
-
         for node_list in graph_nodes:
             graph.add_nodes_from(node_list)
 
@@ -174,3 +177,7 @@ def construct_graph(input_directory, output_directory, file_pattern, graph_name,
 
     program_end = perf_counter()
     print(f'{indent}execution finished in {program_end-program_start:.2f}s\n')
+
+indent = ''
+if __name__ != '__main__':
+    indent = '    '
